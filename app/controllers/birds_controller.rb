921497f93196fc157040f50af4a1575e752088fa
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-21 20:38:08 -0500

    commit tree dfdea1275776c3fdc3a56406d04a8118ae68ef02

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,12 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
+    # @birds = Bird.all
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    # render json: @birds
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # render plain: "Hello #{@birds[3].name}"
+    # render 'birds/index.html.erb'
   end
 end
